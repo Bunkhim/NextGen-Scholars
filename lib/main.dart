29877125a38cf5bbfe5c1c973080952775c368e6
An Bunkhim <bunkhim09@gmail.com>
2026-06-30 12:27:53 +0700

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:scholarship_app/database/database_helper.dart';
 import 'package:scholarship_app/database/seeds/database_seeder.dart';
@@ -156,12 +157,12 @@ class _ScholarshipAppState extends State<ScholarshipApp> {
                                   dt = themed(dt);
                                 }
 
-                                return MaterialApp(
+                                return GetMaterialApp(
                                   title: 'NextGen Scholars',
                                   debugShowCheckedModeBanner: false,
-                                  themeAnimationDuration:
-                                      const Duration(milliseconds: 360),
-                                  themeAnimationCurve: Curves.easeInOutCubic,
+                                  // themeAnimationDuration:
+                                      // const Duration(milliseconds: 360),
+                                  // themeAnimationCurve: Curves.easeInOutCubic,
                                   initialRoute: AppRoutes.splashScreen,
                                   theme: lt,
                                   darkTheme: dt,
